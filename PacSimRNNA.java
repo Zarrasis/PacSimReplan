7c244682344b464acae6bcdfce41d5b8d9f57042
Matt Taubler <Zarrasis@Matts-Air.attlocal.net>
2017-10-05 17:07:51 -0400

    new

---
+++ PacSimRNNA.java
@@ -0,0 +1,18 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import pacsim.BFSPath;
+import pacsim.PacAction;
+import pacsim.PacCell;
+import pacsim.PacFace;
+import pacsim.PacSim;
+import pacsim.PacUtils;
+import pacsim.PacmanCell;
+
+
+public class PacSimRNNA implements PacAction
+{
+	public PacSimRNNA{
+		
+	}
+}
