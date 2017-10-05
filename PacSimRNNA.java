57b118cef46338f5756900384414a3d4358c98e0
Zarrasis <matttaubler@knights.ucf.edu>
2017-10-05 18:05:03 -0400

    changed manifest

--- PacSimRNNA.java
+++ PacSimRNNA.java
@@ -14,23 +14,50 @@ import pacsim.PacmanCell;
 
 public class PacSimRNNA implements PacAction
 {
-	public PacSimRNNA(String fname){
+	private List<Point> path;
+   	private int simTime;
 
 
+	public PacSimRNNA( String fname ) {
+     PacSim sim = new PacSim( fname );
+     sim.init(this);
 	}
 
 	public static void main(String[] args){
 
+		 System.out.println("\nTSP using RNNA by Matthew Taubler and Brooke Norton:");
+		 System.out.println("\nMaze : " + args[ 0 ] + "\n" );
+		 new PacSimRNNA( args[ 0 ] );
 	}
 
 	@Override
-   public void init() {
-      
+   public void init(){
+      	simTime = 0; 
+   		path = new ArrayList();
    }
 
 
    @Override
    public PacFace action( Object state ){
 
+   	 PacCell[][] grid = (PacCell[][]) state;
+     PacmanCell pc = PacUtils.findPacman( grid );
+
+     // Check to see if Pacman is in game 
+     if (pc == null) 
+     	return null;
+     
+     if(path.isEmpty() ){
+     	Point target =PacUtils.nearestFood(pc.getLoc(), grid); 
+     	System.out.println(target);
+
+     }
+
+
+
+   	PacFace face = PacUtils.direction(pc.getLoc(), pc.getLoc());
+   	return face;
+
    }
+
 }
